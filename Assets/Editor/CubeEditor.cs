d82fac6f208556de4f57046b16c70343d8d3c548
Valentino Sparvoli <valensparvoli@gmail.com>
2021-04-27 18:34:29 -0300

    Primer tutorial completo

---
+++ Assets/Editor/CubeEditor.cs
@@ -0,0 +1,27 @@
+﻿using UnityEditor;
+using UnityEngine;
+
+[CustomEditor(typeof(Cube))]
+public class CubeEditor : Editor
+{
+    public override void OnInspectorGUI()
+    {
+        base.OnInspectorGUI();
+
+        Cube cube = (Cube)target;
+
+        GUILayout.BeginHorizontal();
+
+        if(GUILayout.Button("Change Position"))
+        {
+            cube.Movimiento();
+        }
+
+        if(GUILayout.Button("Reset Position"))
+        {
+            cube.Reset();
+        }
+
+        GUILayout.EndHorizontal();
+    }
+}
---
+++ Assets/Scri/Cube.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Cube : MonoBehaviour
+{
+    
+
+    public void Start()
+    {
+        Movimiento();
+    }
+
+    public void Movimiento()
+    {
+        GetComponent<Transform>().position= new Vector3(Random.Range(-10, 10), Random.Range(-10, 10), Random.Range(-10, 10));
+        
+    }
+
+    public void Reset()
+    {
+        GetComponent<Transform>().position = new Vector3(0,0,0);
+    }
+
+
+}
